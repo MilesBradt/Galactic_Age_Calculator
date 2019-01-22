594a7c8e063305ca970eb693f1d4fc7166376b37
Miles Bradt <smbradtmichael@gmail.com>
2019-01-22 14:29:47 -0800

    remove all instances of var and add README

---
+++ README.md
@@ -0,0 +1,18 @@
+## **Galactic Age Calculator**
+
+#### By _**Miles Bradt**_
+
+## Description
+
+_Backend testing specs only so far: set up functions to take a user input for their age and life expectancy and will convert their age based on the planets Mercury through Jupiter. Will also convert how many years they have left based off life expectancy and if they are over expectancy, how many years over they are._
+
+## Setup/Installation Requirements
+
+* Clone GitHub URL in terminal
+* Type "npm install" and wait for everything to finish installing
+* Type "npm run test"
+* Wait for broser to open then click debug to see all tests passing.
+
+## Known Bugs
+
+* None so far
--- spec/calc-spec.js
+++ spec/calc-spec.js
@@ -3,126 +3,114 @@ import { Calc } from './../src/calc.js';
 describe('Calc', function () {
 
   it('returns user age in Mercury years, .24 Earth years', function() {
-    var age = 26;
+    const age = 26;
     const mercury = new Calc();
-    var output = mercury.mercuryYears(age);
+    let output = mercury.mercuryYears(age);
     expect(output).toEqual(6.24);
   });
 
   it('returns user age in Venus years, .62 Earth years', function() {
-    var age = 26;
+    const age = 26;
     const venus = new Calc();
-    var output = venus.venusYears(age);
+    let output = venus.venusYears(age);
     expect(output).toEqual(16.12);
   });
 
   it('returns user age in Mars years, 1.88 Earth years', function() {
-    var age = 26;
+    const age = 26;
     const mars = new Calc();
-    var output = mars.marsYears(age);
+    let output = mars.marsYears(age);
     expect(output).toEqual(48.88);
   });
 
   it('returns user age in Jupiter years, 11.86 Earth years', function() {
-    var age = 26;
+    const age = 26;
     const jupiter = new Calc();
-    var output = jupiter.jupiterYears(age);
+    let output = jupiter.jupiterYears(age);
     expect(output).toEqual(308.36);
   });
 
   it('determines how many years the user has left on Mercury', function() {
-    var age = 26;
-    var lifeExpectancy = 85;
+    const age = 26;
+    const lifeExpectancy = 85;
     const mercury = new Calc();
-    var convertedAge = mercury.mercuryYears(age);
-    var output = mercury.mercuryYearsLeft(lifeExpectancy, convertedAge);
+    let convertedAge = mercury.mercuryYears(age);
+    let output = mercury.mercuryYearsLeft(lifeExpectancy, convertedAge);
     expect(output).toEqual(14.16);
   });
 
   it('determines how many years the user has left on Venus', function() {
-    var age = 26;
-    var lifeExpectancy = 85;
+    const age = 26;
+    const lifeExpectancy = 85;
     const venus = new Calc();
-    var convertedAge = venus.venusYears(age);
-    var output = venus.venusYearsLeft(lifeExpectancy, convertedAge);
+    let convertedAge = venus.venusYears(age);
+    let output = venus.venusYearsLeft(lifeExpectancy, convertedAge);
     expect(output).toEqual(36.58);
   });
 
   it('determines how many years the user has left on Mars', function() {
-    var age = 26;
-    var lifeExpectancy = 85;
+    const age = 26;
+    const lifeExpectancy = 85;
     const mars = new Calc();
-    var convertedAge = mars.marsYears(age);
-    var output = mars.marsYearsLeft(lifeExpectancy, convertedAge);
+    let convertedAge = mars.marsYears(age);
+    let output = mars.marsYearsLeft(lifeExpectancy, convertedAge);
     expect(output).toEqual(110.92);
   });
 
   it('determines how many years the user has left on Jupiter', function() {
-    var age = 26;
-    var lifeExpectancy = 85;
+    const age = 26;
+    const lifeExpectancy = 85;
     const jupiter = new Calc();
-    var convertedAge = jupiter.jupiterYears(age);
-    var output = jupiter.jupiterYearsLeft(lifeExpectancy, convertedAge);
+    let convertedAge = jupiter.jupiterYears(age);
+    let output = jupiter.jupiterYearsLeft(lifeExpectancy, convertedAge);
     expect(output).toEqual(699.74);
   });
 
   it('determines how many years over the life expentancy they are on Mercury', function() {
-    var age = 90;
-    var lifeExpectancy = 85;
+    const age = 90;
+    const lifeExpectancy = 85;
     const mercury = new Calc();
-    if (age >= lifeExpectancy) {
-      var convertedAge = mercury.mercuryYears(age);
-      var convertedLifeExpectancy = mercury.mercuryYears(lifeExpectancy)
-      var output = mercury.mercuryLifeOverExpectancy(convertedAge, convertedLifeExpectancy);
-    } else {
-    var convertedAge = mercury.mercuryYears(age);
-    var output = mercury.mercuryYearsLeft(lifeExpectancy, convertedAge);
-    }
+
+    let convertedAge = mercury.mercuryYears(age);
+    let convertedLifeExpectancy = mercury.mercuryYears(lifeExpectancy)
+    let output = mercury.mercuryLifeOverExpectancy(convertedAge, convertedLifeExpectancy);
+  
     expect(output).toEqual(1.2);
   });
 
   it('determines how many years over the life expentancy they are on Venus', function() {
-    var age = 90;
-    var lifeExpectancy = 85;
+    const age = 90;
+    const lifeExpectancy = 85;
     const venus = new Calc();
-    if (age >= lifeExpectancy) {
-      var convertedAge = venus.venusYears(age);
-      var convertedLifeExpectancy = venus.venusYears(lifeExpectancy)
-      var output = venus.venusLifeOverExpectancy(convertedAge, convertedLifeExpectancy);
-    } else {
-    var convertedAge = venus.venusYears(age);
-    var output = venus.venusYearsLeft(lifeExpectancy, convertedAge);
-    }
+
+    let convertedAge = venus.venusYears(age);
+    let convertedLifeExpectancy = venus.venusYears(lifeExpectancy)
+    let output = venus.venusLifeOverExpectancy(convertedAge, convertedLifeExpectancy);
+
     expect(output).toEqual(3.1);
   });
 
   it('determines how many years over the life expentancy they are on Mars', function() {
-    var age = 90;
-    var lifeExpectancy = 85;
+    const age = 90;
+    const lifeExpectancy = 85;
     const mars = new Calc();
-    if (age >= lifeExpectancy) {
-      var convertedAge = mars.marsYears(age);
-      var convertedLifeExpectancy = mars.marsYears(lifeExpectancy)
-      var output = mars.marsLifeOverExpectancy(convertedAge, convertedLifeExpectancy);
-    } else {
-    var convertedAge = mars.marsYears(age);
-    var output = mars.marsYearsLeft(lifeExpectancy, convertedAge);
-    }
+
+    let convertedAge = mars.marsYears(age);
+    let convertedLifeExpectancy = mars.marsYears(lifeExpectancy)
+    let output = mars.marsLifeOverExpectancy(convertedAge, convertedLifeExpectancy);
+
     expect(output).toEqual(9.4);
   });
 
   it('determines how many years over the life expentancy they are on Jupiter', function() {
-    var age = 90;
-    var lifeExpectancy = 85;
+    const age = 90;
+    const lifeExpectancy = 85;
     const jupiter = new Calc();
-    if (age >= lifeExpectancy) {
-      var convertedAge = jupiter.jupiterYears(age);
-      var convertedLifeExpectancy = jupiter.jupiterYears(lifeExpectancy)
-      var output = jupiter.jupiterLifeOverExpectancy(convertedAge, convertedLifeExpectancy);
-    } else {
-    var convertedAge = jupiter.jupiterYears(age);
-    var output = jupiter.jupiterYearsLeft(lifeExpectancy, convertedAge);
-    }
+
+    let convertedAge = jupiter.jupiterYears(age);
+    let convertedLifeExpectancy = jupiter.jupiterYears(lifeExpectancy)
+    let output = jupiter.jupiterLifeOverExpectancy(convertedAge, convertedLifeExpectancy);
+
     expect(output).toEqual(59.3);
   });
 });
